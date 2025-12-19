c69e907f01b1aff328ba2ceb0a6e1bac1e29b053
vararais <varadigmaisnann@gmail.com>
2025-12-20 01:08:46 +0700

    Menambah dan membuat API untuk siswa

---
+++ app/src/main/java/com/example/questapi_049/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.questapi_049.apiservice
+
+import com.example.questapi_049.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface ServiceApiSiswa {
+    @GET(value = "bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST(value = "insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+}
