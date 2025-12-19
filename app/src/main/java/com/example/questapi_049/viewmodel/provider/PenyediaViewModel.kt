9d2ea92b775adfcc2e953372dd65c2744d0da85a
vararais <varadigmaisnann@gmail.com>
2025-12-20 01:08:12 +0700

    Menambah dan membuat Penyedia View Model

---
+++ app/src/main/java/com/example/questapi_049/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.questapi_049.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi_049.repositori.AplikasiDataSiswa
+import com.example.questapi_049.viewmodel.EntryViewModel
+import com.example.questapi_049.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+    }
+}
