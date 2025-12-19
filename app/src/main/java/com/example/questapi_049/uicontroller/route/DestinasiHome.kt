4f023e6293a4afa6d651b0b19116547c20751f49
vararais <varadigmaisnann@gmail.com>
2025-12-20 01:06:06 +0700

    Menambah dan membuat Destinasi Home

---
+++ app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_049.uicontroller.route
+
+import com.example.questapi_049.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
