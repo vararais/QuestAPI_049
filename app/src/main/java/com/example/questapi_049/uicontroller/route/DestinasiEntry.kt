9d40d8bbb453cb9bd30c760d23f714fd863cfae1
vararais <varadigmaisnann@gmail.com>
2025-12-20 01:05:58 +0700

    Menambah dan membuat Destinasi Entry

---
+++ app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_049.uicontroller.route
+
+import com.example.questapi_049.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
