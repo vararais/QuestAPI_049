262d8e590b001e6e0c1fb91f356b51efadeeafdb
vararais <varadigmaisnann@gmail.com>
2025-12-19 14:47:59 +0700

    Menambah dependensi lifecycle dan navigasi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
