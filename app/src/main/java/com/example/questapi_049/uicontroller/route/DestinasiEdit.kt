47087beac8e5ebf3d33432a01e94a11a4ce9cd61
vararais <varadigmaisnann@gmail.com>
2025-12-27 00:00:52 +0700

    Perbaikan destinasi edit

--- app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_049.uicontroller.route
 
+import com.example.questapi_049.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "edit"
+    override val titleRes = R.string.edit_siswa
+    const val idSiswa = "id"
+    val routeWithArgs = "$route/{$idSiswa}"
+}
