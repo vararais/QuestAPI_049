97e8b90113c8339d5be58d94e129c60c722615bb
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:26:37 +0700

    Menambah override suspend fun untuk get satu siswa

--- app/src/main/java/com/example/questapi_049/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_049/repositori/RepositoryDataSiswa.kt
@@ -18,4 +18,6 @@ class JaringanRepositoryDataSiswa(
 ) : RepositoryDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
