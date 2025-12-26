86e9d8564b5dac01df80912e1c731271db432f4e
vararais <varadigmaisnann@gmail.com>
2025-12-27 00:01:00 +0700

    Perbaikan destinasi navigasi

--- app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiNavigasi.kt
@@ -1,22 +1,6 @@
 package com.example.questapi_049.uicontroller.route
 
-import com.example.questapi_049.R
-
 interface DestinasiNavigasi {
     val route: String
     val titleRes: Int
-}
-
-object DestinasiDetail : DestinasiNavigasi {
-    override val route = "detail"
-    override val titleRes = R.string.detail_siswa
-    const val idArg = "id"
-    val routeWithArgs = "$route/{$idArg}"
-}
-
-object DestinasiEdit : DestinasiNavigasi {
-    override val route = "edit"
-    override val titleRes = R.string.edit_siswa
-    const val idArg = "id"
-    val routeWithArgs = "$route/{$idArg}"
 }
