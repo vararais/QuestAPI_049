3a357dbe3438b57c6cebc1a41c1efa3c47612515
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:24:22 +0700

    Update repository update data siswa

--- app/src/main/java/com/example/questapi_049/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_049/repositori/RepositoryDataSiswa.kt
@@ -8,6 +8,7 @@ interface RepositoryDataSiswa {
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
 
     suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun updateDataSiswa(id: Int, dataSiswa: DataSiswa): Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
