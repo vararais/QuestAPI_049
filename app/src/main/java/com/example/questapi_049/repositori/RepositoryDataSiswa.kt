353bad4036097acc7f2ce657942945c8c4f34cc4
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:25:23 +0700

    Tambah import

--- app/src/main/java/com/example/questapi_049/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_049/repositori/RepositoryDataSiswa.kt
@@ -2,6 +2,7 @@ package com.example.questapi_049.repositori
 
 import com.example.questapi_049.apiservice.ServiceApiSiswa
 import com.example.questapi_049.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
