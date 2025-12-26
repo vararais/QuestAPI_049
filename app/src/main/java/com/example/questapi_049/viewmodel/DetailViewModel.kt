969e69f42741054c445a3c9fcc9b1d50dd004db5
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:28:50 +0700

    Menambah sealed interface detailuistate

--- app/src/main/java/com/example/questapi_049/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_049/viewmodel/DetailViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.questapi_049.viewmodel
 
+
+
+sealed interface DetailUiState {
+    data class Success(val dataSiswa: DataSiswa) : DetailUiState
+    object Error : DetailUiState
+    object Loading : DetailUiState
+}
+
 class DetailViewModel {
 }
