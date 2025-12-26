bccfb53f2407fb178b3dd1d7a6817923fd1e8a04
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:39:28 +0700

    Menambahkan object destinasi detail

--- app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiNavigasi.kt
@@ -1,6 +1,15 @@
 package com.example.questapi_049.uicontroller.route
 
+import com.example.questapi_049.R
+
 interface DestinasiNavigasi {
     val route: String
     val titleRes: Int
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = R.string.detail_siswa
+    const val idArg = "id"
+    val routeWithArgs = "$route/{$idArg}"
 }
