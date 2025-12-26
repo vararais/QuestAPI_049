a14766e7e4ca1a95b3537106e22e494067455509
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:41:48 +0700

    Membuat halaman edit

--- app/src/main/java/com/example/questapi_049/uicontroller/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_049/uicontroller/view/HalamanEdit.kt
@@ -1,2 +1,51 @@
 package com.example.questapi_049.uicontroller.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_049.uicontroller.route.DestinasiEdit
+import com.example.questapi_049.viewmodel.EditViewModel
+import com.example.questapi_049.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = "Edit Siswa",
+                canNavigateBack = true,
+                navigateUp = onNavigateUp,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            uiStateSiswa = viewModel.editUiState,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
