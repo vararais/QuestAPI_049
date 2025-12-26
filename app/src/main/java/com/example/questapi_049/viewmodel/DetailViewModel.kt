29e4162da10d91fd86dabac07db46d3d17db0ecb
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:31:43 +0700

    Menambah import

--- app/src/main/java/com/example/questapi_049/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_049/viewmodel/DetailViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.questapi_049.viewmodel
 
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_049.modeldata.DataSiswa
+import com.example.questapi_049.repositori.RepositoryDataSiswa
+import com.example.questapi_049.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface DetailUiState {
     data class Success(val dataSiswa: DataSiswa) : DetailUiState
