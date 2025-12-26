b877eddb0097311b55d5caa8cf3c13c7af3e7d25
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:32:46 +0700

    Membuat class editviewmodel

--- app/src/main/java/com/example/questapi_049/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_049/viewmodel/EditViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.questapi_049.viewmodel
 
-class EditViewModel {
-}
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var editUiState by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val _id: Int = checkNotNull(savedStateHandle[DestinasiEdit.idArg])
+
+    init {
+        viewModelScope.launch {
+            editUiState = repositoryDataSiswa.getSatuSiswa(_id).toUiStateSiswa(true)
+        }
+    }
