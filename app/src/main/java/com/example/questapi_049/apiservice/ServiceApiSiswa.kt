9c032feab824b537193da8ec1055fe9204362119
vararais <varadigmaisnann@gmail.com>
2025-12-26 21:21:18 +0700

    Update Api Get Baca1Teman

--- app/src/main/java/com/example/questapi_049/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_049/apiservice/ServiceApiSiswa.kt
@@ -1,7 +1,9 @@
 package com.example.questapi_049.apiservice
 
 import com.example.questapi_049.modeldata.DataSiswa
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -13,4 +15,9 @@ interface ServiceApiSiswa {
 
     @POST(value = "insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @GET("baca1Teman.php")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+
 }
