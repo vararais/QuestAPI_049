43261b11b68bdde9a250b432eb7592134a89e9e1
vararais <varadigmaisnann@gmail.com>
2025-12-27 00:00:44 +0700

    Perbaikan destinasi detail

--- app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_049/uicontroller/route/DestinasiDetail.kt
@@ -1,4 +1,10 @@
 package com.example.questapi_049.uicontroller.route
 
-class DestinasiDetail {
+import com.example.questapi_049.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = R.string.detail_siswa
+    const val idSiswa = "id"
+    val routeWithArgs = "$route/{$idSiswa}"
 }
